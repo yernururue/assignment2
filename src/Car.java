52fb20dfdf600dd4149b2e5671f33718558adc69
yernururue <250367@astanait.edu.kz>
2025-12-29 22:12:09 +0500

    added car subclass

---
+++ src/Car.java
@@ -0,0 +1,19 @@
+public abstract class Car extends Vehicle {
+
+    int doors;
+    public String fueltype;
+
+    public Car(String brand, int year) {
+        super(brand, year);
+    }
+
+    @Override
+    public void startEngine() {
+
+    }
+
+    @Override
+    public void stopEngine() {
+
+    }
+}
