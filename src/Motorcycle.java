cced3a1992d5bc6ecfee16c2b08c10d17c301b07
yernururue <250367@astanait.edu.kz>
2025-12-29 22:19:54 +0500

    added Motorcycle, Truck subclass. (draft)

---
+++ src/Motorcycle.java
@@ -0,0 +1,17 @@
+public class Motorcycle extends Vehicle{
+    boolean hasSidecar;
+
+    public Motorcycle(String brand, int year) {
+        super(brand, year);
+    }
+
+    @Override
+    public void startEngine() {
+
+    }
+
+    @Override
+    public void stopEngine() {
+
+    }
+}
---
+++ src/Truck.java
@@ -0,0 +1,18 @@
+public class Truck extends Vehicle{
+    double capacity;
+    int numAxles;
+    public Truck(String brand, int year) {
+        super(brand, year);
+    }
+
+    @Override
+    public void startEngine() {
+
+    }
+
+    @Override
+    public void stopEngine() {
+
+    }
+}
+
